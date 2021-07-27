11f7279505ccdd602fddfb83fc5828f58226375c
Damian Babiarz <babiarz_damian@wp.pl>
2021-07-28 00:36:30 +0200

    feat: add user features

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send({msg: 'Welcome to the Contact Keeper API'}));
 
 // define routes
